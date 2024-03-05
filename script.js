0ebd8120d5407d4a7bfc8f02a3e36a14074c3e68
Max Johansson <142883892+TE22B-Johansson-Max@users.noreply.github.com>
2024-03-05 15:03:34 +0100

    Klar (Hade inte tid för att göra dropdown)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const products = document.getElementById("product-container");
+const cart = document.getElementById("CartItems");
 var cars = [];
 var ItemsInCart =  [];
 
@@ -7,9 +8,21 @@ function AddToCart(i) {
     localStorage.setItem("ItemsInCart", JSON.stringify(ItemsInCart));
 }
 
+function Cart() {
+    var itemsInCart = JSON.parse(localStorage.getItem("ItemsInCart"));
+    var cartHTML = document.getElementById("CartItems");
+    cartHTML.innerHTML = "";
+    itemsInCart.forEach(car => {
+        cartHTML.innerHTML += "<li>" + car.name + " - " + car.price + " kr" + "</li>"; 
+    });
+
+}
+
 fetch("./cars.json")
 .then((response) => response.json())
 .then((json) => {
     cars = json.cars;
     loadProducts();
     });
+
+document.onload = Cart()
